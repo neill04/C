418e7677246125611e99f7a4cc1b0e51c9a3d2b2
Alfredo Paz <apazv@unsa.edu.pe>
2024-10-09 12:24:41 -0500

    buildOneTwoThree con solo 7 asignaciones

--- list.c
+++ list.c
@@ -9,22 +9,15 @@ int length(struct Node* head){
   return count;
 }
 struct Node* buildOneTwoThree(){
-  struct Node* head = NULL;
-  struct Node* second = NULL;
-  struct Node* third = NULL;
-
-  head = malloc(sizeof(struct Node));
-  second = malloc(sizeof(struct Node));
-  third = malloc(sizeof(struct Node));
-
+  struct Node* head = malloc(sizeof(struct Node));
   head->data = 1;
-  head->next = second;
+  head->next = malloc(sizeof(struct Node));
+  
+  head->next->data = 2;
+  head->next->next = malloc(sizeof(struct Node));
 
-  second->data = 2;
-  second->next = third;
-
-  third->data = 3;
-  third->next = NULL;
+  head->next->next->data = 3;
+  head->next->next->next = NULL;
 
   return head;
 }
